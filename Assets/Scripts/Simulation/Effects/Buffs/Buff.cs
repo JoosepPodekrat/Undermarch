2a1db35ebe4a0bace3dc4110ff16a996ece1e0d8
ChadenProjects <chadenblizzard@protonmail.com>
2025-10-26 21:37:57 +0200

    Safe non vibecoded branch incase it explodes

--- Assets/Scripts/Simulation/Effects/Buffs/Buff.cs
+++ Assets/Scripts/Simulation/Effects/Buffs/Buff.cs
@@ -3,15 +3,108 @@ namespace Undermarch
     public class Buff : ICharacterEffect
     {
         public string name { get; set; }
+        public string description { get; set; }
         public int duration { get; set; }
+
         public void Add(Character target)
-        {
-        }
-        public void Remove(Character target) { }
-        public void Apply(Character target)
         {
             target.buffs.Add(this);
         }
 
+        public void Remove(Character target)
+        {
+            target.buffs.Remove(this);
+        }
+
+        // Allow subclasses to define custom behavior
+        public virtual void Apply(Character target) { }
+    }
+
+    public class AgilityBuff1 : Buff
+    {
+        public AgilityBuff1()
+        {
+            name = "Agility Buff Rank 1";
+            description = "Increases Agility by 1.";
+            duration = 30;
+        }
+
+        public override void Apply(Character target)
+        {
+            target.agility += 1;
+        }
+    }
+
+    public class IntelligenceBuff1 : Buff
+    {
+        public IntelligenceBuff1()
+        {
+            name = "Intelligence Buff Rank 1";
+            description = "Increases Intelligence by 1.";
+            duration = 30;
+        }
+
+        public override void Apply(Character target)
+        {
+            target.intelligence += 1;
+        }
+    }
+
+    public class StaminaBuff1 : Buff
+    {
+        public StaminaBuff1()
+        {
+            name = "Stamina Buff Rank 1";
+            description = "Increases Stamina by 1.";
+            duration = 30;
+        }
+
+        public override void Apply(Character target)
+        {
+            target.stamina += 1;
+        }
+    }
+
+    public class StrengthBuff1 : Buff
+    {
+        public StrengthBuff1()
+        {
+            name = "Strength Buff Rank 1";
+            description = "Increases Strength by 1.";
+            duration = 30;
+        }
+
+        public override void Apply(Character target)
+        {
+            target.strength += 1;
+        }
+    }
+
+    public class SpiritBuff1 : Buff
+    {
+        public SpiritBuff1()
+        {
+            name = "Spirit Buff Rank 1";
+            description = "Increases Spirit by 1.";
+            duration = 30;
+        }
+
+        public override void Apply(Character target)
+        {
+            target.spirit += 1;
+        }
+    }
+    public class Renew1 : Buff
+    {
+        public Renew1()
+        {
+            name = "Renew Rank 1";
+            description = "Heals the target for 1 hp per tick.";
+            duration = 30;
+        }
+        public override void Apply(Character target)
+        {
+            target.currentHP += 1;
+        }
     }
 }
--- Assets/Scripts/Simulation/Effects/Debuffs/Debuff.cs
+++ Assets/Scripts/Simulation/Effects/Debuffs/Debuff.cs
@@ -3,16 +3,112 @@ namespace Undermarch
     public class Debuff : ICharacterEffect
     {
         public string name { get; set; }
+        public string description { get; set; }
         public int duration { get; set; }
+
         public void Add(Character target)
-        {
-        }
-        public void Remove(Character target) { }
-        public void Apply(Character target)
         {
             target.debuffs.Add(this);
         }
 
+        public void Remove(Character target)
+        {
+            target.debuffs.Remove(this);
+        }
 
+        // Allow subclasses to define custom behavior
+        public virtual void Apply(Character target) { }
+    }
+
+    public class AgilityDebuff1 : Debuff
+    {
+        public AgilityDebuff1()
+        {
+            name = "Agility Debuff Rank 1";
+            description = "Decreases Agility by 1.";
+            duration = 30;
+        }
+
+        public override void Apply(Character target)
+        {
+            target.agility -= 1;
+        }
+    }
+
+    public class IntelligenceDebuff1 : Debuff
+    {
+        public IntelligenceDebuff1()
+        {
+            name = "Intelligence Debuff Rank 1";
+            description = "Decreases Intelligence by 1.";
+            duration = 30;
+        }
+
+        public override void Apply(Character target)
+        {
+            target.intelligence -= 1;
+        }
+    }
+
+    public class StaminaDebuff1 : Debuff
+    {
+        public StaminaDebuff1()
+        {
+            name = "Stamina Debuff Rank 1";
+            description = "Decreases Stamina by 1.";
+            duration = 30;
+        }
+
+        public override void Apply(Character target)
+        {
+            target.stamina -= 1;
+        }
+    }
+
+    public class StrengthDebuff1 : Debuff
+    {
+        public StrengthDebuff1()
+        {
+            name = "Strength Debuff Rank 1";
+            description = "Decreases Strength by 1.";
+            duration = 30;
+        }
+
+        public override void Apply(Character target)
+        {
+            target.strength -= 1;
+        }
+    }
+
+    public class SpiritDebuff1 : Debuff
+    {
+        public SpiritDebuff1()
+        {
+            name = "Spirit Debuff Rank 1";
+            description = "Decreases Spirit by 1.";
+            duration = 30;
+        }
+
+        public override void Apply(Character target)
+        {
+            target.spirit -= 1;
+        }
+    }
+
+    public class Corruption1 : Debuff
+    {
+        public Corruption1()
+        {
+            name = "Corruption Rank 1";
+            description = "Deals 1 damage to the target per tick.";
+            duration = 30;
+        }
+
+        public override void Apply(Character target)
+        {
+            target.currentHP -= 1;
+            if (target.currentHP < 0)
+                target.currentHP = 0;
+        }
     }
 }
