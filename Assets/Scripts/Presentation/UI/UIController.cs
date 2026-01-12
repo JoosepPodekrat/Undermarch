7a3b647d9653b19d8b14878860c4842962336dea
ChadenProjects <joosep.podekrat@ut.ee>
2026-01-12 18:11:10 +0200

    Added new Main menu, needs to be finished

---
+++ Assets/Scripts/Presentation/UI/UIController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("Main Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject subMenus;
+
+    [Header("Sub Menu Panels")]
+    public GameObject optionsPanel;
+    public GameObject levelPanel;
+    public GameObject newGamePanel;
+
+    public void OpenOptions()
+    {
+        mainMenuPanel.SetActive(false);
+        subMenus.SetActive(true);
+
+        optionsPanel.SetActive(true);
+        levelPanel.SetActive(false);
+        newGamePanel.SetActive(false);
+    }
+
+    public void OpenMainMenu()
+    {
+        subMenus.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void OpenLevelLoader()
+    {
+        mainMenuPanel.SetActive(false);
+        subMenus.SetActive(true);
+
+        optionsPanel.SetActive(false);
+        levelPanel.SetActive(true);
+        newGamePanel.SetActive(false);
+    }
+
+    public void OpenNewGame()
+    {
+        mainMenuPanel.SetActive(false);
+        subMenus.SetActive(true);
+
+        optionsPanel.SetActive(false);
+        levelPanel.SetActive(false);
+        newGamePanel.SetActive(true);
+    }
+}
